dddfb0aed98ed703b5dfdf8a4cc47a1d16c60458
ryanfok <ryanfok@cs.washington.edu>
2020-01-14 21:08:34 -0800

    Reset password responds when email is submitted

--- lib/screens/authentication/resetPassword.dart
+++ lib/screens/authentication/resetPassword.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterfire_test/screens/authentication/authenticate.dart';
 import 'package:flutterfire_test/services/firebaseAuth.dart';
 
@@ -13,15 +14,26 @@ class ResetPassword extends StatefulWidget {
 class _ResetPassword extends State<ResetPassword> {
   var email = '';
   var error = '';
+  var emailSent = false;
 
   AuthService _auth = AuthService();
   final _resetPasswordFormKey = GlobalKey<FormState>();
 
   void _handleResetPassword() {
+    setState(() => error = '');
     if (_resetPasswordFormKey.currentState.validate()) {
       _auth.resetPassword(email).then((value) {
+        setState(() {
+          emailSent = true;
+        });
         print('Password reset email sent to $email');
       }).catchError((e) {
+        setState(() {
+          error = 'There is no user corresponding to the given email.';
+        });
+        print('User not found: $email');
+      }, test: (e) => e is PlatformException
+      ).catchError((e) {
         setState(() {
           error = e.message;
         });
@@ -32,12 +44,27 @@ class _ResetPassword extends State<ResetPassword> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return emailSent ? 
+    Scaffold(
       appBar: AppBar(
         title: Text('Reset Password'),
         actions: <Widget>[
           FlatButton(
-            child: Text('Return to Login Screen'),
+            child: Text('Login'),
+            onPressed: () => widget.goToAuthMethod(LoginMethod.loginWithEmailAndPasssword),
+          )
+        ],
+      ),
+      body: Center(
+        child: Text('If an account exists, an email will be sent with instructions.')
+      )
+    )
+    : Scaffold(
+      appBar: AppBar(
+        title: Text('Reset Password'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Login'),
             onPressed: () => widget.goToAuthMethod(LoginMethod.loginWithEmailAndPasssword),
           )
         ],
@@ -68,11 +95,13 @@ class _ResetPassword extends State<ResetPassword> {
                   ),
                   Text(
                     error,
-                    style: TextStyle(color: Colors.red)
+                    style: TextStyle(color: Colors.red, height: 2),
+                    textAlign: TextAlign.left,
                   ),
                   RaisedButton(
-                    child: Text('Reset password'),
+                    child: Text('Email me a recovery link'),
                     onPressed: _handleResetPassword,
+                    
                   )
                 ]
               )
