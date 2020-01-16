802b3ca1bfcf6cb285ede9c204acff112f1b0d01
ryanfok <ryanfok@cs.washington.edu>
2020-01-15 19:24:24 -0800

    First commit for polling function

---
+++ lib/screens/polls/optionWithVoters.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class OptionWithVoters extends StatelessWidget {
+  OptionWithVoters({
+    this.option,
+    this.voters,
+    this.voterPercent
+  });
+
+  final String option;
+  final BigInt voters;
+  final double voterPercent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: OutlineButton(
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget> [
+                  Text(
+                    option,
+                    style: TextStyle(fontSize: 18)
+                  ),
+                  Text(
+                    '${(voterPercent * 100).toStringAsFixed(2)}%',
+                    style: TextStyle(fontSize: 18, color: Colors.black54)
+                  )
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 3),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.cyan[300],
+                    width: 1
+                  )
+                ),
+                child: LinearProgressIndicator(
+                  value: voterPercent,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.cyan[300]),
+                  backgroundColor: Colors.white,
+                )
+              ),
+              Text(
+                '$voters Voters',
+                style: TextStyle(fontSize: 12, color: Colors.black54)
+              )
+            ],
+          )
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        disabledBorderColor: Colors.blue,
+        disabledTextColor: Colors.black,
+      )
+    );
+  }
+}
---
+++ lib/screens/polls/optionWithoutVoters.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+typedef void OptionPressedCallback(String option);
+
+class OptionWithoutVoters extends StatelessWidget {
+  OptionWithoutVoters({
+    this.option,
+    this.onOptionPressed
+  });
+
+  final String option;
+  final OptionPressedCallback onOptionPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: OutlineButton(
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text(option),
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        borderSide: BorderSide(color: Colors.blue),
+        // TODO: onPressed
+        onPressed: () => onOptionPressed(option)
+      )
+    );
+  }
+}
